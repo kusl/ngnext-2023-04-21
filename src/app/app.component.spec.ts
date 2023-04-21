12b6b75651bade373a575c3d0dff6b0b98f108d9
Kushal Hada <kus@tilde.team>
2023-04-21 07:15:57 -0400

    add timestamp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule,MatSlideToggleModule],
     declarations: [AppComponent]
   }));
 
---
+++ update.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+cd ~/src/angular/ngnext-2023-04-21;
+ping -A -D -c 20 -v 8.8.8.8 > ping.txt;
+date > README.md;
+git add .;
+git commit -m "add timestamp";
+
+echo ''  >> README.md 2>&1;
+echo '# Angular Fifteen'  >> README.md 2>&1;
+echo ''  >> README.md 2>&1;
+
+echo ''  >> README.md 2>&1;
+echo 'This project is live at [https://angularfifteen.github.io](https://angularfifteen.github.io "fifteen!") thanks to Github.'  >> README.md 2>&1;
+echo ''  >> README.md 2>&1;
+
+echo "\`\`\`bash"  >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin add system status";
+echo "System Memory"  >> README.md 2>&1;
+free -h  >> README.md 2>&1;
+echo "System Storage"  >> README.md 2>&1;
+du -sh . >> README.md 2>&1;
+git add .;
+git commit -m "add system status";
+echo "\`\`\`"  >> README.md 2>&1;
+git add .;
+git commit -m "end add system status";
+
+
+echo "\`\`\`bash"  >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin update node";
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh;  # This loads NVM
+source ~/.nvm/nvm.sh;
+time nvm install --lts;
+time nvm use --lts;
+export NODE_OPTIONS="--max-old-space-size=8000"; time npm install --global @angular/cli yarn;
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng version  >> README.md 2>&1;
+time yarn  >> README.md 2>&1;
+echo "\`\`\`"  >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "end update node";
+
+export NODE_OPTIONS="--max-old-space-size=8000";
+
+git add .;
+git commit -m "begin yarn";
+time yarn;
+git add .;
+git commit -m "end yarn";
+
+git add .;
+git commit -m "begin prepare to update angular";
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng update @angular/core @angular/cli @angular/material @angular/localize;
+git add .;
+git commit -m "end prepare to update angular";
+time npx browserslist --update-db >> README.md 2>&1;
+
+echo "\`\`\`bash" >> README.md 2>&1;
+time npx browserslist --update-db >> README.md 2>&1;
+echo "\`\`\`" >> README.md 2>&1;
+
+echo "\`\`\`bash"  >> README.md 2>&1;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin prepare to build angular";
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng build --configuration production --output-path docs --output-hashing none --stats-json true --subresource-integrity true --verbose true  >> README.md 2>&1;
+echo "\`\`\`"  >> README.md 2>&1;
+git add .;
+git commit -m "end prepare to build angular";
+
+echo "\`\`\`bash" > locallog/fedoratest.md;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "begin prepare to unit test angular";
+export NODE_OPTIONS="--max-old-space-size=8000"; time yarn run ng test --karma-config karma.conf.js >> locallog/fedoratest.md;
+echo "\`\`\`" >> locallog/fedoratest.md;
+ping -A -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+git add .;
+git commit -m "end prepare to unit test angular";
+
+date  >> README.md 2>&1;
+ping -D -c 20 -v 8.8.8.8 >> ping.txt 2>&1;
+time yarn version --patch  >> README.md 2>&1;
+git add .;
+git commit -m "add timestamp";
+git pull --rebase origin development --strategy-option=ours;
+git add .;
+git commit -m "merge from remote";
+git push origin development;
