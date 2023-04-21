6641ee407ecf93ba83bf31f29bf4fea133113bf8
Kushal Hada <kus@tilde.team>
2023-04-21 07:19:47 -0400

    add package
    time yarn add --dev karma-jasmine karma-jasmine karma-chrome-launcher karma-jasmine-html-reporter karma-coverage-istanbul-reporter karma-firefox-launcher karma-jasmine-html-reporter karma-spec-reporter karma-verbose-reporter
    yarn add v1.22.19
    [1/4] Resolving packages...
    [2/4] Fetching packages...
    [3/4] Linking dependencies...
    warning "@angular-devkit/build-angular > postcss-loader > cosmiconfig-typescript-loader@4.3.0" has unmet peer dependency "@types/node@*".
    warning "@angular-devkit/build-angular > postcss-loader > cosmiconfig-typescript-loader@4.3.0" has unmet peer dependency "ts-node@>=10".
    [4/4] Building fresh packages...
    
    success Saved lockfile.
    success Saved 5 new dependencies.
    info Direct dependencies
    ├─ karma-chrome-launcher@3.2.0
    ├─ karma-coverage-istanbul-reporter@3.0.3
    ├─ karma-firefox-launcher@2.1.2
    ├─ karma-spec-reporter@0.0.36
    └─ karma-verbose-reporter@0.0.8
    info All dependencies
    ├─ karma-chrome-launcher@3.2.0
    ├─ karma-coverage-istanbul-reporter@3.0.3
    ├─ karma-firefox-launcher@2.1.2
    ├─ karma-spec-reporter@0.0.36
    └─ karma-verbose-reporter@0.0.8
    Done in 5.30s.
    
    real    0m5.561s
    user    0m4.222s
    sys     0m0.747s

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-// time yarn add --dev karma-jasmine karma-jasmine karma-chrome-launcher karma-jasmine-html-reporter karma-coverage-istanbul-reporter karma-firefox-launcher karma-jasmine-html-reporter karma-spec-reporter
+// time yarn add --dev karma-jasmine karma-jasmine karma-chrome-launcher karma-jasmine-html-reporter karma-coverage-istanbul-reporter karma-firefox-launcher karma-jasmine-html-reporter karma-spec-reporter karma-verbose-reporter
 // yarn add v1.22.19
 // [1/4] Resolving packages...
 // [2/4] Fetching packages...
